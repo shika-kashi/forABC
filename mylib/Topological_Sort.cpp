0dcb932b30aec7fca620640a471c31d8229dfc3f
shika-kashi <t.daiki.silvio.5353@gmail.com>
2025-07-20 19:11:02 +0900

    Create Topological_Sort.cpp

---
+++ mylib/Topological_Sort.cpp
@@ -0,0 +1,29 @@
+//トポロジカルソート topological sort
+vecl TS(graph g)
+{
+    ll n = g.size() ;
+    set<pll>st;//入次数、id
+    vector<ll> ee(n , 0) ;
+    rep(i,0,n){
+        for(ll x : g[i]){
+            ee[x] ++ ;
+        }
+    }
+    rep(i,0,n){
+        st.insert({ee[i] , i}) ;
+    }
+    vector<ll> ret ;
+    while(!st.empty()){
+        ll i = (*st.begin()).second ;
+        st.erase(st.begin()) ;
+        //iを削除する
+        for(ll x : g[i]){
+            if(st.find({ee[x] , x}) == st.end())continue ;
+            st.erase({ee[x] , x}) ;
+            ee[x] -- ;
+            st.insert({ee[x] , x});
+        }
+        ret.push_back(i) ;
+    }
+    return ret; 
+}
