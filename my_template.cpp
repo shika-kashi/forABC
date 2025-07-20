ed96c77fcf217a81e44e0fb85765d5d0174ac5db
shika-kashi <t.daiki.silvio.5353@gmail.com>
2025-07-20 19:01:21 +0900

    Create my_template.cpp

---
+++ my_template.cpp
@@ -0,0 +1,52 @@
+#include<bits/stdc++.h>
+using namespace std;
+using ll = long long;//int型は使わない
+using vecl = vector<ll>;
+using graph = vector<vector<ll>>;
+using pll = pair<ll , ll>;
+const ll inf = (1LL<<61); //約2e18
+const ll MOD = 998244353;
+const vecl delx={1,0,-1,1,-1,1,0,-1};
+const vecl dely={1,1,1,0,0,-1,-1,-1};
+#define rep(i,a,b) for(ll i=(ll)a; i<(ll)b; i++)
+#define rrep(i,a,b) for(ll i=(ll)b-1; i>=(ll)a; i--)
+#define all(vec1) (vec1).begin(), (vec1).end()
+#define yn(boolean) if(boolean){cout << "Yes" << endl;}else{cout << "No" << endl;}
+
+//あまり(負の数対応)
+template<typename T>
+T ovr(T a,T b){
+  T ret=a%b;
+  if(ret<0)ret+=b;
+  return ret;
+}
+const string MOD_bi = "111111111111111111111110110111";
+//MOD下での逆元
+ll minv(ll ina){
+  ll a = ina % MOD;
+  ll ret = 1;
+  ll V = a;
+  rep(i,0,MOD_bi.size()){
+    if(MOD_bi[i]=='1')ret=(ret*V)%MOD;
+    V=(V*V)%MOD;
+  }
+  return ret;
+}
+
+//指数をある値で割った余り
+ll mpow(ll a , ll b , ll M){
+    ll ret = 1;
+    ll V = a;
+    rep(i,0,64){
+        if((b >> i) & 1)ret=(ret*V)%M;
+        V=(V*V)%M;
+    }
+    return ret;
+}
+
+/////////main/////////
+
+int main()
+{
+    
+}
