b521d8eb7b184d9c488edc635683b21cca58237e
aa36 <t.daiki.silvio.5353@gmail.com>
2025-07-31 19:50:52 +0900

    Update BIT.cpp

--- mylib/BIT.cpp
+++ mylib/BIT.cpp
@@ -1,7 +1,7 @@
 //普通のBIT(遅延セグ木はACLを使う)
 //使用例：https://atcoder.jp/contests/tessoku-book/submissions/66834445
 struct BIT{
-    vecl tre;
+    vl tre;
     ll n;
     BIT(ll N) : tre(N+1){
         rep(i,0,N+1)tre[i]=0;
